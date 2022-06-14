fc409e12201568344e9bd5d323f79d3806eb5357
AbdulrehmanTs <rohi644@gmail.com>
2022-06-15 03:16:14 +0500

    localization

--- pages/ar/privacy-policy.js
+++ pages/ar/privacy-policy.js
@@ -1,13 +1,16 @@
 import React from "react";
-import Hero from "../components/hero/hero";
-import Layout from "../components/layout/layout";
-import ShopByCategory from "../components/shop-by-category/shopByCategory";
-import TextImgSection from "../components/text-img-section/textImgSection";
-import katwalkBag from "../assets/images/designers/katwalk-bag.jpg";
-import Features from "../components/features/features";
-import HeadingAndText from "../components/heading-and-text/headingAndText";
-
+import Hero from "./components/hero/hero";
+import Layout from "./components/layout/layout";
+import ShopByCategory from "./components/shop-by-category/shopByCategory";
+import TextImgSection from "./components/text-img-section/textImgSection";
+import katwalkBag from "../../assets/images/designers/katwalk-bag.jpg";
+import Features from "./components/features/features";
+import HeadingAndText from "./components/heading-and-text/headingAndText";
+import { useEffect } from "react";
 function PrivacyPolicy() {
+  useEffect(() => {
+    document.querySelector("body").style.direction = 'rtl'
+  }, [])
   const privacyPolicyContent = [
     {
       title: "1. Introduction",

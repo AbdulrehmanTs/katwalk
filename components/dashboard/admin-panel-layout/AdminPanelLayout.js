853096d3355bdcee69daaaa6da6ba33b84a1a0cf
AbdulrehmanTs <rohi644@gmail.com>
2022-06-16 00:23:52 +0500

    sidebar

--- components/dashboard/admin-panel-layout/AdminPanelLayout.js
+++ components/dashboard/admin-panel-layout/AdminPanelLayout.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import styles from "./AdminPanelLayout.module.css";
 import BottomBar from "./bottom-bar/BottomBar";
-import SideBar from "./sidebar/sideBar";
+import SideBar from "./sidebar/SideBar";
 
 const AdminPanelLayout = ({ children, footer, active }) => {
   const [showSideBar, setShowSideBar] = useState(false);
